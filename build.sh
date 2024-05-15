d4965fe37d7a021cd2089c51c2476ee749a756dc
Josef Leyva <jalm_x@hotmail.com>
2024-05-15 16:45:08 -0600

    build release Wed May 15 04:45:08 PM CST 2024

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ HASH=$(git rev-parse --short HEAD)
 rm -rf release 2&> /dev/null
 
 pyinstaller --noconfirm --onefile --console --name "generate_info_lasted" --add-data "src:src/" --paths "src"  "src/__main__.py" && \ 
-rm -rf build && \
-mv dist release && \
-cp release/generate_info_lasted release/generate_info_$HASH && \
-echo "release created"
+    rm -rf build && \
+    rm generate_info*
+    mv dist release && \
+    cp release/generate_info_lasted "release/generate_info_$HASH" && \
+    echo "release created -> generate_info_$HASH"
