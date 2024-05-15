ec3b96ad9d5786dca6ef9cb2b672cae40506b4db
Josef Leyva <jalm_x@hotmail.com>
2024-05-15 16:49:36 -0600

    build release Wed May 15 04:49:36 PM CST 2024

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 source venv/bin/activate
 
+pip install pyinstaller 2&> /dev/null
+
 git add . 
 DATE=$(date)
 git commit -m "build release $DATE"
@@ -9,9 +11,8 @@ git commit -m "build release $DATE"
 HASH=$(git rev-parse --short HEAD)
 rm -rf release generate_info* 2&> /dev/null
 
-# pyinstaller --noconfirm --onefile --console --name "generate_info_lasted" --add-data "src:src/" --paths "src"  "src/__main__.py" && \ 
+pyinstaller --noconfirm --onefile --console --name "generate_info_lasted" --add-data "src:src/" --paths "src"  "src/__main__.py" && \ 
     rm -rf build && \
-    rm generate_info* && \
     mv dist release && \
     cp -r release/generate_info_lasted "release/generate_info_$HASH" && \
     echo "release created -> generate_info_$HASH"
